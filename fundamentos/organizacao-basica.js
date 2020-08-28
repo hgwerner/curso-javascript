4bc9508d9f28aad685b1c8e860ac81d8f0d903eb
hgwerner <hgwerner777@gmail.com>
2020-08-28 17:27:20 -0300

    adicionado os fundamentos e console

---
+++ fundamentos/organizacao-basica.js
@@ -0,0 +1,8 @@
+console.log('Ola mundo! Eu sou a senteça Javascript')
+
+    {
+        {
+            console.log('Ola mundo! Eu sou um bloco de codigos Javascript')
+            console.log('Ola mundo! Eu sou um novo bloco de codigos Javascript')
+        }
+    }
